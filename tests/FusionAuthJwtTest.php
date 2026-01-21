da177236c43cdbe6c27802206d83e35b46fced06
danilopolani <danilo.polani@gmail.com>
2026-01-21 10:52:42 +0100

    handle token audience as array

--- tests/FusionAuthJwtTest.php
+++ tests/FusionAuthJwtTest.php
@@ -46,3 +46,47 @@ test('::validate() will not throw anything if audience matches the client_id', f
     // Basic expectation to avoid the warning
     expect(true)->toBeTrue();
 });
+
+test('::validate() will not throw anything if at least one token audience matches a valid audience as array', function () {
+    Config::set('fusionauth.audience', ['foo', 'bar']);
+    Config::set('fusionauth.issuers', ['foo', 'bar']);
+
+    FusionAuthJwt::validate((object) ['aud' => ['baz', 'bar'], 'iss' => 'foo']);
+    FusionAuthJwt::validate((object) ['aud' => 'bar', 'iss' => 'foo']);
+
+    // Basic expectation to avoid the warning
+    expect(true)->toBeTrue();
+});
+
+
+test('::validate() will not throw anything if at least one token audience matches a valid audience as string', function () {
+    Config::set('fusionauth.audience', 'bar');
+    Config::set('fusionauth.issuers', ['foo', 'bar']);
+
+    FusionAuthJwt::validate((object) ['aud' => ['baz', 'bar'], 'iss' => 'foo']);
+    FusionAuthJwt::validate((object) ['aud' => 'bar', 'iss' => 'foo']);
+
+    // Basic expectation to avoid the warning
+    expect(true)->toBeTrue();
+});
+
+test('::validate() will throw an error if token audience as string does not match a valid audience', function () {
+    Config::set('fusionauth.audience', ['foo', 'bar']);
+    Config::set('fusionauth.issuers', ['foo', 'bar']);
+
+    FusionAuthJwt::validate((object) ['aud' => 'qux', 'iss' => 'foo']);
+})->throws(InvalidTokenException::class);
+
+test('::validate() will throw an error if token audience as array does not match a valid audience as string', function () {
+    Config::set('fusionauth.audience', 'foo');
+    Config::set('fusionauth.issuers', ['foo', 'bar']);
+
+    FusionAuthJwt::validate((object) ['aud' => ['baz', 'qux'], 'iss' => 'foo']);
+})->throws(InvalidTokenException::class);
+
+test('::validate() will throw an error if token audience as array does not match a valid audience as array', function () {
+    Config::set('fusionauth.audience', ['foo', 'bar']);
+    Config::set('fusionauth.issuers', ['foo', 'bar']);
+
+    FusionAuthJwt::validate((object) ['aud' => ['baz', 'qux'], 'iss' => 'foo']);
+})->throws(InvalidTokenException::class);
